1b1708f435d77f1581670c517c89a250e8151b1e
Abhay <abhay@cybersify.tech>
2024-11-12 15:00:52 +0530

    code push

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -199,7 +199,6 @@ const onSubmit=()=>{
           </div>
         </div>
 
-        {/* Right Section (Summary) */}
         <div className="flex-1 mt-6 md:mt-0 md:ml-6">
           <div className="bg-gray-50 p-6 rounded-lg shadow-md">
             <h2 className="text-lg font-bold mb-4">Your Card</h2>
