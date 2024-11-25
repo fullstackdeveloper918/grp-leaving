ece607275a9655c25a3522a007bb89ff149aecea
abhishek <fullstackdeveloper918@gmail.com>
2024-11-25 15:32:28 +0530

    banner design

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,12 @@
+import DemoCard from '@/components/DemoCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <DemoCard/>
+    </div>
+  )
+}
+
+export default page
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@
 import Images from "@/constants/images";
 import { List } from "antd";
 import Image from "next/image";
+import card_absolute from "../assets/images/card_absolute.png"
+// import
 import React, { useEffect, useState } from "react";
 import cardData from "../constants/CardJson/card.json";
 const categoriesName = ['Farewell', 'Birthday', 'Baby', 'Wedding', 'Get Well', 'Sympathy', 'Thank you', 'Retirement', 'Congratulations', 'Anniversary', 'Welcome', 'New Home'];
@@ -37,22 +39,23 @@ console.log(displayedText,"displayedText");
 
   return (
     <>
-      <section className="bg-heroColor  dark:bg-gray-900 heroSectionHeight  align-middle d-flex homeBanner">
-        <div className="container-fluid  py-8 mx-auto  xl:gap-0 lg:py-16 d-flex justify-between  lg:flex-nowrap flex-wrap space-y-10 items-center">
-          <div className="lg:w-1/2 w-full md:text-center xs:text-center lg:text-left ">
-            <h1 className="bannerHeaderH1">
+      <section className="  dark:bg-gray-900  bg-cover bg-no-repeat  align-middle  homeBanner ">
+       <div className="relative bg-hero_banner_new heroSectionHeight">
+       <div className="container-fluid  py-8 mx-auto  xl:gap-0 lg:py-16  space-y-10 items-center">
+          <div className=" mx-auto text-center w-full md:text-center xs:text-center lg:text-left ">
+            <h1 className="bannerHeaderH1 text-center max-w-[50%] mx-auto">
               Group Greeting Cards{" "}
-              <span className="text-blueText">for {displayedText||"''"}</span>
+              <span className="text-white">for {displayedText||"''"}</span>
             </h1>
-            <p className="text-blackText font-medium">
+            <p className="text-white text-center font-medium mx-auto ">
             The simplest way to share a virtual greeting card with everyone in your office.
             </p>
 
-            <ul className="list-none d-flex xl:pl-7 lg:px-5 gap-10">
-              <li className="relative listBefore ml-4 md:text-lg lg:text-lg font-medium">Easy to share</li>
-              <li className="relative listBefore ml-5 md:text-lg lg:text-lg font-medium">Quick to create </li>
-              <li className="relative listBefore ml-5 md:text-lg lg:text-lg font-medium">Unlimited signatures</li>
-            </ul>
+            {/* <ul className="list-none flex xl:pl-7 lg:px-5 gap-10 justify-center ">
+              <li className="relative listBefore ml-4 md:text-lg lg:text-lg font-medium text-white">Easy to share</li>
+              <li className="relative listBefore ml-5 md:text-lg lg:text-lg font-medium text-white">Quick to create </li>
+              <li className="relative listBefore ml-5 md:text-lg lg:text-lg font-medium text-white">Unlimited signatures</li>
+            </ul> */}
 
            <div className="bannerButton">
            <a
@@ -60,22 +63,24 @@ console.log(displayedText,"displayedText");
               className=" btnPrimary"
             > Start a group Card</a>
             <a
-              href="#"
+              href="/demo"
               className="btnSecondary ml-3"
             >              Try Our Demo Card
             </a>
            </div>
           </div>
-          <div className="lg:w-1/2 w-full">
-            <Image
+          {/* <div className="lg:w-1/2 w-full"> */}
+            {/* <Image
               src={Images.Hero}
               width={900}
               height={900}
               alt="hero_image"
-            />
-          </div>
+            /> */}
+          {/* </div> */}
         </div>
-      </section>
+        <img src={card_absolute.src} className="absolute_img" alt="img card"/>
+       </div>
+        </section>
     </>
   );
 };
