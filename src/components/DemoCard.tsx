44b9d18a690239aaf600167f4b05d9f4f55cc637
fullstack developer <fullstackdeveloper918@gmail.com>
2025-01-31 19:03:18 +0530

    editor

--- src/components/DemoCard.tsx
+++ src/components/DemoCard.tsx
@@ -13,6 +13,7 @@ import DemoBoard from "./common/DemoBoard";
 import userIcon from "../assets/icons/ab.png"
 import Image from 'next/image';
 import Custom from "./common/custom";
+import Customcraousal from "./common/Customcraousal";
 const { Paragraph, Text } = Typography;
 const DemoCard = ({params}:any) => {
   const [show, setShow] = useState<any>(false);
@@ -103,7 +104,8 @@ const DemoCard = ({params}:any) => {
               </div>
 
               {/* {show ? <EditorModal showCard={showCard}/> : <EditorCrousal />} */}
-              {show ? <EditorModal/> : <Custom />}
+              {show ? <EditorModal/> : <Customcraousal />}
+              {/* {show ? <EditorModal/> : <Custom />} */}
               {/* <Carousel /> */}
               {/* <EditorModal/> */}
             </div>
